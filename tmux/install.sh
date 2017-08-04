a6f0fb5aa3ba46f1ac5b8bdf8cd03c9d8867a5f2
Chris Buryta <chris@buryta.com>
2017-08-03 23:43:15 -0400

    clean up tmux before configure tpm. install tpm if not installed

---
+++ tmux/install.sh
@@ -0,0 +1,11 @@
+# we'll need to make sure ruby + chefdk is installed first, or we may need sudo
+# gem install tmuxinator
+
+# try tmux-continum, and use tpm for tmux plugin management
+mkdir -p $ZSH/tmux/plugins
+
+if [ ! -d $ZSH/tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm $ZSH/tmux/plugins/tpm
+fi
+
+

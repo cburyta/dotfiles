e5398d7ff24d0ca538df704fd6d969544d43862f
Chris Buryta <chris@buryta.com>
2023-02-11 23:03:12 -0500

    git - default to 'main' branch, system aliases - minor tweaks

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -7,3 +7,30 @@ then
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
 fi
+
+# exa overides for ls
+#   `brew install exa`
+if $(exa &>/dev/null)
+then
+  # general use
+  #
+  # ls
+  alias ls='exa'
+  # list, size, type, git
+  alias l='exa -lbF --git'
+  # long list
+  alias ll='exa -lbGF --git'
+  # long list, modified date sort
+  alias llm='exa -lbGd --git --sort=modified'
+  # all list
+  alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'
+  # all + extended list
+  alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale'
+
+  # specialty views
+  #
+  # one column, just names
+  alias lS='exa -1'
+  # tree
+  alias lt='exa --tree --level=2'
+fi

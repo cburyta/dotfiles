120629209ced9e0d7870ad0cdb470e5a7abc07cd
Chris Buryta <chris@buryta.com>
2025-08-14 22:46:53 -0400

    initial asdf files

---
+++ asdf/completion.zsh
@@ -0,0 +1,5 @@
+# https://asdf-vm.com/guide/introduction.html
+#
+# asdf is a tool version manager. All tool version definitions are contained within one file
+# (.tool-versions) which you can check in to your project's Git repository to share with
+# your team, ensuring everyone is using the exact same versions of tools.

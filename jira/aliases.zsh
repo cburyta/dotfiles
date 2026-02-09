511de17882d05ab09cc68aa897dbd8ba40da13c6
Chris Buryta <chris@buryta.com>
2026-02-09 10:53:07 -0500

    added some jira command shortcuts

---
+++ jira/aliases.zsh
@@ -0,0 +1,4 @@
+alias mybacklog='jira issue list -a$(jira me) -s~Complete'
+alias mytickets='jira issue list -a$(jira me) -q "Sprint in openSprints()"'
+alias sprint='jira issue list -q "Sprint in openSprints()"'
+
